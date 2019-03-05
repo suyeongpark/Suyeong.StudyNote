b817af244b2aa64818029830ce67ea55eab0d188
suyeongpark <suyeongpark@abyne.com>
2019-03-05 23:14:29 +0900

    일단 서버 추가.

---
+++ Suyeong.StudyNote/Suyeong.StudyNote.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Suyeong.StudyNote.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
